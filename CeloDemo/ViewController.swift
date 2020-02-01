755a9fac114a0d653f9546de50e395823e042bea
Suman maharjan <silversunwing@gmail.com>
2020-02-02 12:32:29 +1300

    Initial Commit

---
+++ CeloDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CeloDemo
+//
+//  Created by Crunchyiee on 2/02/20.
+//  Copyright © 2020 Maharjan Suman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
